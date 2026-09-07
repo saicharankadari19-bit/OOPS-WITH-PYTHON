423f0bc64cbae07d2122dd5a62f7553b7cfc0fba
saicharankadari19-bit <saicharankadari19@gmail.com>
2026-09-07 16:04:38 +0530

    Create create a class with deposit.py

---
+++ create a class with deposit.py
@@ -0,0 +1,8 @@
+class Bank:
+    def __init__(self,balance):
+        self.balance = balance
+    def deposit(self,amount):
+        self.balance += amount
+        print("Balance:",self.balance)
+b = Bank(1000)
+b.deposit(2000)
