228e8cf324558488f335e831e348d78c0ebb28f1
saicharankadari19-bit <saicharankadari19@gmail.com>
2026-09-07 15:41:01 +0530

    Create create a class with multiple objects.py

---
+++ create a class with multiple objects.py
@@ -0,0 +1,11 @@
+class Sample:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def read(self):
+        print(self.name)
+        print(self.age)
+s1=Sample("sai",18)
+s1.read()
+s2=Sample("Charan",20)
+s2.read()
